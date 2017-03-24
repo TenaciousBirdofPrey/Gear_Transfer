c0dc140c9f8dccdb7e37848899001d04eb0bc363
deep-howlround <odonnell.abu@gmail.com>
2017-03-24 11:43:52 -0400

    work on hotel db, add to admin

--- firstdjango/transfers/admin.py
+++ firstdjango/transfers/admin.py
@@ -5,9 +5,21 @@ from django.contrib import admin
 # Register your models here.
 from .models import Hotels, Transfers
 
-class HotelAdmin(admin.ModelAdmin):
+class HotelsAdmin(admin.ModelAdmin):
     list_display = ['hotel_name','latt','long']
 class TransfersAdmin(admin.ModelAdmin):
 	list_display = ['from_loc','to_loc','item']
 
-admin.site.register(Transfers,TransfersAdmin)
+
+
+
+#admin.site.register(Transfers,TransfersAdmin)
+admin.register(Hotels,Transfers)(admin.ModelAdmin)
+
+
+
+# # File: admin.py
+# from django.contrib import admin
+# from .models import Project, Client, About
+
+# admin.register(Project, Client, About)(admin.ModelAdmin)
